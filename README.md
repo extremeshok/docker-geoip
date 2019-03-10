7fe04d0891855313b7fdb08b933ad4c4cda92815
eXtremeSHOK <admin@extremeshok.com>
2019-03-10 20:53:20 +0200

    Maxmind GeoIP Legacy Databases (last available version is included)

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ View **docker-compose-sample.yml** in the source repository for usage
 
 nginx-countries by gypthecat
 Maxmind GeoIPv2 Databases
-Maxmind GeoIP Legacy Databases
+Maxmind GeoIP Legacy Databases (last available version is included)
--- rootfs/xshok-geoip.sh
+++ rootfs/xshok-geoip.sh
@@ -3,22 +3,25 @@ echo "========== Updating Maxmind GeoIPv2 Databases =========="
 mkdir -p /geoip/maxmind
 geoipupdate -v -d /geoip/maxmind
 
-echo "========== Updating Maxmind GeoIP Legacy Databases =========="
-mkdir -p /tmp/maxmind-legacy
+# Deprecated, now we include the last available version
+rsync -a -v --ignore-existing "/usr/share/GeoIP" "/tmp/maxmind-legacy/"
 
-if [ -f "/tmp/maxmind-legacy/GeoIP.dat.gz" ] && [ -f "/geoip/maxmind/GeoIP.dat" ]; then
-  curl -o /tmp/maxmind-legacy/GeoIP.dat.gz -z /tmp/maxmind-legacy/GeoIP.dat.gz -L http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
-else
-  curl -o /tmp/maxmind-legacy/GeoIP.dat.gz -L http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
-fi
-gunzip /tmp/maxmind-legacy/GeoIP.dat.gz
-
-if [ -f "/tmp/maxmind-legacy/GeoLiteCity.dat.gz" ] && [ -f "/geoip/maxmind/GeoLiteCity.dat" ]; then
-  curl -o /tmp/maxmind-legacy/GeoLiteCity.dat.gz -z /tmp/maxmind-legacy/GeoLiteCity.dat.gz -L http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-else
-  curl -o /tmp/maxmind-legacy/GeoLiteCity.dat.gz -L http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-fi
-gunzip /tmp/maxmind-legacy/GeoLiteCity.dat.gz
+# echo "========== Updating Maxmind GeoIP Legacy Databases =========="
+# mkdir -p /tmp/maxmind-legacy
+#
+# if [ -f "/tmp/maxmind-legacy/GeoIP.dat.gz" ] && [ -f "/geoip/maxmind/GeoIP.dat" ]; then
+#   curl -o /tmp/maxmind-legacy/GeoIP.dat.gz -z /tmp/maxmind-legacy/GeoIP.dat.gz -L http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+# else
+#   curl -o /tmp/maxmind-legacy/GeoIP.dat.gz -L http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+# fi
+# gunzip /tmp/maxmind-legacy/GeoIP.dat.gz
+#
+# if [ -f "/tmp/maxmind-legacy/GeoLiteCity.dat.gz" ] && [ -f "/geoip/maxmind/GeoLiteCity.dat" ]; then
+#   curl -o /tmp/maxmind-legacy/GeoLiteCity.dat.gz -z /tmp/maxmind-legacy/GeoLiteCity.dat.gz -L http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+# else
+#   curl -o /tmp/maxmind-legacy/GeoLiteCity.dat.gz -L http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+# fi
+# gunzip /tmp/maxmind-legacy/GeoLiteCity.dat.gz
 
 rsync -W -h -r -L -p -t -g -o -i --prune-empty-dirs --exclude "*.mmdb" --no-compress "/tmp/maxmind-legacy/" "/geoip/maxmind/"
 
